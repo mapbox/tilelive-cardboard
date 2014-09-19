50d985cc2b3b6d3a9082d5cd806fc824f7cc57bb
Ryan Clark <ryan.clark.j@gmail.com>
2014-09-19 11:03:29 -0700

    use event emitter to manage locked out jobs

--- test/cache.test.js
+++ test/cache.test.js
@@ -21,7 +21,7 @@ process.env.AWS_SECRET_ACCESS_KEY = 'fake';
 process.env.AWS_DEFAULT_REGION = 'fake';
 
 test('load data', function(t) {
-    // Eight lines, one in each of eight adjacent z6 tiles. 
+    // Eight lines, one in each of eight adjacent z6 tiles.
     // Parent tiles are 5/14/15 and 5/15/15
     // Each line contains 5000 vertices, so each z5 tile should be over 500kb,
     // and so minzoom should be z5
@@ -35,7 +35,7 @@ test('load data', function(t) {
     });
 });
 
-var cache;
+var cache, tilejson;
 
 test('calculate info', function(t) {
     var cardboard = Cardboard(config);
@@ -47,8 +47,9 @@ test('calculate info', function(t) {
                 t.ifError(err, 'got info');
                 t.equal(info.minzoom, 5, 'expected minzoom');
                 cache = new Cache(info, config.dataset, config);
+                tilejson = info;
                 src.close(t.end.bind(t));
-            })
+            });
         });
     });
 });
@@ -58,10 +59,41 @@ test('getTile', function(t) {
         t.ifError(err, 'got tile');
         var key = tilebelt.tileToQuadkey([14, 15, 5]);
         var badKey = tilebelt.tileToQuadkey([15, 15, 5]);
-        t.ok(cache[key], 'bridge obj cached for parent tile');
-        t.notOk(cache[badKey], 'no extra bridge obj cached');
+        t.ok(cache._bridges[key], 'bridge obj cached for parent tile');
+        t.notOk(cache._bridges[badKey], 'no extra bridge obj cached');
         t.end();
     });
+});
+
+test('locking', function(t) {
+    var loadAttempts = 0;
+    var getTileResponses = 0;
+    var expectedErrors = 0;
+
+    function mockLoader(bounds, dataset, callback) {
+        loadAttempts++;
+        setTimeout(function() {
+            callback(new Error('Failed to load data'));
+        }, 100);
+    }
+
+    function done() {
+        t.equal(loadAttempts, 2, 'tried to load each root tile only once');
+        t.equal(expectedErrors, 8, 'failed all requests');
+        t.end();
+    }
+
+    var badCache = new Cache(tilejson, config.dataset, config, mockLoader);
+
+    for (var x = 28; x < 32; x++) {
+        for (var y = 30; y < 32; y++) {
+            badCache.getTile(6, x, y, function(err, tile) {
+                if (err) expectedErrors++;
+                getTileResponses++;
+                if (getTileResponses === 8) done();
+            });
+        }
+    }
 });
 
 test('close database', function(t) {
@@ -69,7 +101,7 @@ test('close database', function(t) {
 });
 
 test('getTile from cache', function(t) {
-    // Should be able to load data from a sibling
+    // Should be able to load data from a sibling, even though dynalite is off
     cache.getTile(6, 29, 30, function(err, tile) {
         t.ifError(err, 'loaded tile from cached bridge');
         t.end();
@@ -88,6 +120,6 @@ test('getTile fail where data is missing', function(t) {
 test('close cache', function(t) {
     cache.close(function(err) {
         t.ifError(err, 'closed cache');
-        t.end(); 
+        t.end();
     });
 })
